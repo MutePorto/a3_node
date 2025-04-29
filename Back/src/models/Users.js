f73af1500bbc3e9addd20d9bdfa1892dc8895019
Carlos-CesarP <carloscpnovais@gmail.com>
2025-04-29 10:07:35 -0300

    fazendo conexao com o banco de dados

---
+++ Back/src/models/Users.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/sequelize');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  tipo: {
+    type: DataTypes.ENUM('admin', 'cliente'),
+    allowNull: false
+  }
+}, {
+  tableName: 'users',
+  timestamps: false
+});
+
+module.exports = User;
