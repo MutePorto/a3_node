a1e7285429c1b5aa2875f785c7d660beca8c8093
Murilo Teixeira <mutegbi@gmail.com>
2025-04-29 09:23:35 -0300

    Adicioando tempo a funcionalidade das tabelas e modficando o cabeçalho dinamico

--- Front/assets/js/axios/user.js
+++ Front/assets/js/axios/user.js
@@ -1,22 +1,34 @@
-const url = "http://localhost:3000/" // URL do servidor
+const url = "https://lightgrey-louse-782029.hostingersite.com/" // URL do servidor
 
-const bt = document.getElementById('body-table-user') // ID da tabela onde os dados serão exibidos
+const bt = document.getElementById('body-table-motorista') // ID da tabela onde os dados serão exibidos
 
 function getUser() { // Função para obter os dados do servidor    
     bt.innerHTML = '';
-    axios.get(url) // Fazendo uma requisição GET para o servidor
+    axios.get(url + 'getUser.php') // Fazendo uma requisição GET para o servidor
         .then(response => { // Quando a requisição for bem-sucedida
             console.log(response) // Exibindo a resposta no console
             const dados = response.data // Armazenando os dados da resposta em uma variável
             console.log(dados)  // Exibindo os dados no console
             console.log(dados.length) // Exibindo o tamanho dos dados no console
 
+            bt.innerHTML = '' // Limpa o conteúdo da tabela antes de adicionar novos dados
             // Criando o cabeçalho da tabela
 
             for (let index = 0; index < dados.length; index++) {
                 bt.innerHTML += '<tr><td>' + dados[index]['nome'] +
                     '</td><td>' + dados[index]['senha'] +
-                    '</td><td>' + dados[index]['email'] + '</td></tr>'
+                    '</td><td>' + dados[index]['email'] +
+                    '</td><td>' + dados[index]['email'] +
+                    `<td>
+                    <div class="w-50">
+                        <button class="btn-link btn-primary" onclick="editUser(${dados[index]['id']})">
+                        <i class="fa fa-edit"></i>
+                        </button>
+                        <button class="btn-link btn-danger" onclick="deleteUser(${dados[index]['id']})">
+                        <i class="fa fa-times"></i>
+                        </button>
+                    </div>
+                    </td></tr>`
             }
         })
         .catch(error => { console.log(error) })
--- Front/assets/js/php/api_php.js
+++ Front/assets/js/php/api_php.js
@@ -1,41 +1,60 @@
 const url = "https://lightgrey-louse-782029.hostingersite.com/"
 
-const bt = document.getElementById('body-table-motorista') // ID da tabela onde os dados serão exibidos
+const btm = document.getElementById('body-table-motorista') // ID da tabela onde os dados serão exibidos
 
 function getUser() {
 
     $.ajax({
         url: `${url}getUser.php`,
         method: 'POST',
-        success: (dados) => {
-            console.log(dados)
+        success: (dados) => { // Função para obter os dados do servidor
             let res = JSON.parse(dados)
             console.log(res.length)
 
-            bt.innerHTML = '' // Limpa o conteúdo da tabela antes de adicionar novos dados
+            btm.innerHTML = '' // Limpa o conteúdo da tabela antes de adicionar novos dados
 
             // Criando o cabeçalho da tabela
-
-            for (let index = 0; index < dados.length; index++) {
-                bt.innerHTML += '<tr><td>' + res[index]['nome'] +
+            for (let index = 0; index < res.length; index++) {
+                btm.innerHTML += '<tr><td>' + res[index]['nome'] +
                     '</td><td>' + res[index]['senha'] +
                     '</td><td>' + res[index]['email'] +
                     '</td><td>' + res[index]['email'] +
-                    `</td><td>
-                    <div class="h-50 w-50">
-                        <button onclick="${res[index]['id']}" class="btn-link btn-primary" data-original-title="Edit Task">
-                            <i class="fa fa-edit"></i>
+                    `<td>
+                    <div class="w-50">
+                        <button class="btn-link btn-primary" onclick="editUser(${res[index]['id']})">
+                        <i class="fa fa-edit"></i>
                         </button>
-                        <button onclick="${res[index]['id']}" class="btn-link btn-danger" data-original-title="Remove">
-                            <i class="fa fa-times"></i>
+                        <button class="btn-link btn-danger" onclick="deleteUser(${res[index]['id']})">
+                        <i class="fa fa-times"></i>
                         </button>
-                    </div></td></tr>`
+                    </div>
+                    </td></tr>`
             }
         }
     })
 }
 
 getUser();
+
+function editUser(id) {
+    console.log(id + ' edit')
+}
+
+function deleteUser(id) { // Função para deletar um usuário
+    console.log(id + ' delete')
+    // $.ajax({
+    //     url: `${url}deleteUser.php`,
+    //     method: 'POST',
+    //     data: { id: id },
+    //     dataType: 'json'
+    // }).done(function (result) {
+    //     console.log(result)
+    //     getUser() // Atualiza a tabela após a exclusão
+    // })
+    //     .fail(function (error) {
+    //         console.error('Erro ao deletar usuário:', error)
+    //     })
+}
 
 $('#btn').click(() => { // Insere um novo usuario no banco de dados
     let nomev = document.getElementById('nome').value
