b91f8ce2c7918ff52f5a529f1cba9bda09663d95
Murilo Teixeira <mutegbi@gmail.com>
2025-05-27 15:26:02 -0300

    Criando estruturação para criar e receber eventos

--- Back/src/routes/eventosRoutes.js
+++ Back/src/routes/eventosRoutes.js
@@ -6,6 +6,42 @@ const Carro = require('../models/Carros');
 const Motorista = require('../models/Motoristas');
 const { Op } = require('sequelize');
 
+// Um evento pertence a um motorista
+Evento.belongsTo(Motorista, {
+  foreignKey: 'id_motorista',
+  as: 'motorista'
+});
+
+// Um evento pertence a um carro
+Evento.belongsTo(Carro, {
+  foreignKey: 'id_carro',
+  as: 'carro'
+});
+
+// GET / para listar eventos
+router.get('/', async (req, res) => {
+  try {
+    const eventos = await Evento.findAll({
+      include: [
+        {
+          model: Motorista,
+          as: 'motorista',
+          attributes: ['id', 'nome', 'status']
+        },
+        {
+          model: Carro,
+          as: 'carro',
+          attributes: ['id', 'marca', 'modelo', 'kmAtual', 'status']
+        }
+      ],
+      order: [['data_inicial', 'DESC']]
+    });
+    res.json(eventos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erro ao buscar eventos.' });
+  }
+});
 
 // GET /disponiveis para criação de evento
 router.get('/disponiveis', async (req, res) => {
@@ -53,17 +89,33 @@ router.get('/disponiveis', async (req, res) => {
   }
 });
 
+// GET / para buscar o km atual do carro
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const carro = await Carro.findByPk(id);
+
+    if (!carro) {
+      return res.status(404).json({ error: 'Carro não encontrado.' });
+    }
+
+    res.json(carro);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const { id_motorista, id_carro, data_inicial, km_inicial } = req.body;
 
     const motorista = await Motorista.findByPk(id_motorista);
-    if (!motorista || motorista.status !== 'ativo') {
+    if (!motorista || motorista.status !== 'Livre') {
       return res.status(400).json({ error: 'Motorista inválido ou inativo.' });
     }
 
     const carro = await Carro.findByPk(id_carro);
-    if (!carro || carro.status !== 'disponivel') {
+    if (!carro || carro.status !== 'Liberado') {
       return res.status(400).json({ error: 'Carro inválido ou indisponível.' });
     }
 
@@ -74,7 +126,8 @@ router.post('/', async (req, res) => {
       km_inicial
     });
 
-    carro.status = 'ocupado';
+    carro.status = 'Usando';
+    motorista.status = 'Indisponivel';
     await carro.save();
 
     res.status(201).json(novoEvento);
--- Front/assets/js/axios/eventos.js
+++ Front/assets/js/axios/eventos.js
@@ -1,12 +1,170 @@
+function getEventos() {
 
-async function buscarDisponiveis() {
-    try {
-        const response = await axios.get(url + 'eventos/disponiveis');
-        const { motoristas, carros } = response.data;
+    axios.get(url + 'eventos')
+        .then(response => {
+            function formatarDataISO(dataISO) {
+                return dataISO.replace('T', ' ').split('.')[0]; // ex: "2025-05-27 14:00:00"
+            }
+            const eventos = response.data; // Armazena os dados da resposta em uma variável
+            const tabelaEventos = document.getElementById('body-table-evento'); // ID da tabela onde os dados serão exibidos
+            tabelaEventos.innerHTML = ''; // Limpa a tabela antes de preencher
+            eventos.forEach(evento => {
+                const dataInicial = formatarDataISO(evento.data_inicial);
+                const dataFinal = evento.data_final ? formatarDataISO(evento.data_final) : 'Em andamento';
 
-        console.log('Motoristas disponíveis:', motoristas);
-        console.log('Carros disponíveis:', carros);
-    } catch (error) {
-        console.error('Erro ao buscar disponíveis:', error.response?.data || error.message);
+                if (!evento.data_final) {
+                    tabelaEventos.innerHTML += `
+                    <tr class="table-warning">                        
+                        <td>${evento.motorista.nome}</td>
+                        <td>${evento.carro.marca} ${evento.carro.modelo}</td>
+                        <td>${dataInicial}</td>
+                        <td>${dataFinal}</td>                        
+                        <td>
+                        <div class="w-50">
+                            <button class="btn-link btn-primary" onclick="editarEvento(${evento.id})"><i class="fa fa-edit"></i></button>
+                            <button class="btn-link btn-danger" onclick="deletarEvento(${evento.id})"><i class="fa fa-times"></i></button>
+                        </div>
+                        </td>
+                    </tr>
+                `;
+                } else {
+                    tabelaEventos.innerHTML += `
+                    <tr>                        
+                        <td>${evento.motorista.nome}</td>
+                        <td>${evento.carro.marca} ${evento.carro.modelo}</td>
+                        <td>${dataInicial}</td>
+                        <td>${dataFinal}</td>                        
+                        <td>
+                        <div class="w-50">
+                            <button class="btn-link btn-primary" onclick="editarEvento(${evento.id})"><i class="fa fa-edit"></i></button>
+                            <button class="btn-link btn-danger" onclick="deletarEvento(${evento.id})"><i class="fa fa-times"></i></button>
+                        </div>
+                        </td>
+                    </tr>
+                `;
+                }
+
+            });
+            $("#events-datatables").DataTable({}); // Inicializa o DataTable após adicionar os dados
+        })
+        .catch(error => {
+            console.error('Erro ao buscar eventos:', error.response?.data || error.message);
+            swal(`Erro ao buscar eventos: ${error.message}`, {
+                icon: "error",
+                buttons: {
+                    confirm: {
+                        className: "btn btn-danger",
+                    },
+                },
+            });
+        });
+}
+getEventos(); // Chama a função para carregar os eventos ao iniciar a página
+
+function preencherSelectsDisponiveis() {
+    axios.get(url + 'eventos/disponiveis')
+        .then(response => {
+            const { motoristas, carros } = response.data;
+
+            console.log('Motoristas disponíveis:', motoristas);
+            console.log('Carros disponíveis:', carros);
+
+            const selectMotoristas = document.getElementById('selectMotoristas');
+            const selectCarros = document.getElementById('selectCarros');
+
+            // Limpar selects
+            selectMotoristas.innerHTML = '<option value="">Selecione um motorista</option>';
+            selectCarros.innerHTML = '<option value="">Selecione um carro</option>';
+
+            // Preencher motoristas
+            motoristas.forEach(motorista => {
+                selectMotoristas.innerHTML += `
+                    <option value="${motorista.id}">
+                        ${motorista.nome} 
+                    </option>
+                `;
+            });
+
+            // Preencher carros
+            carros.forEach(carro => {
+                selectCarros.innerHTML += `
+                    <option value="${carro.id}">
+                        ${carro.marca} ${carro.modelo}
+                    </option>
+                `;
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao buscar disponíveis:', error.response?.data || error.message);
+        });
+}
+
+function receberKmAtual() {
+    const selectCarros = document.getElementById('selectCarros').value; // Obtendo o valor do select de carros
+
+    if (selectCarros) {
+        axios.get(url + 'eventos/' + selectCarros)
+            .then(response => {
+                const carro = response.data;
+                document.getElementById('km_inicial').value = carro.kmAtual;
+            })
+            .catch(error => {
+                console.error('Erro ao buscar KM atual:', error.response?.data || error.message);
+            });
+    } else {
+        document.getElementById('kmAtual').value = '';
     }
+}
+
+function setEvento() {
+    const motoristaId = document.getElementById('selectMotoristas').value; // Obtendo o ID do motorista selecionado
+    const carroId = document.getElementById('selectCarros').value; // Obtendo o ID do carro selecionado
+    const data_inicial = document.getElementById('data_inicial').value; // Obtendo a data inicial
+    const kmInicial = document.getElementById('km_inicial').value; // Obtendo o KM inicial
+
+
+    if (!motoristaId || !carroId || !kmInicial || !data_inicial) {
+        swal("Por favor, preencha todos os campos.", {
+            icon: "warning",
+            buttons: {
+                confirm: {
+                    className: "btn btn-warning",
+                },
+            },
+        });
+        return;
+    }
+
+    axios.post(url + 'eventos', { id_motorista: motoristaId, id_carro: carroId, km_inicial: kmInicial, data_inicial: data_inicial })
+        .then(response => {
+            console.log(response);
+            swal(`Evento cadastrado com sucesso!`, {
+                icon: "success",
+                buttons: {
+                    confirm: {
+                        className: "btn btn-success",
+                    },
+                },
+            })
+                .then(() => {
+                    getEventos(); // Atualiza a lista de eventos
+                    $('#modalEventos').modal('hide'); // Fecha o modal após o cadastro
+
+                });
+        })
+        .catch(error => {
+            console.error(error);
+            swal(`Erro ao cadastrar evento: ${error.message}`, {
+                icon: "error",
+                buttons: {
+                    confirm: {
+                        className: "btn btn-danger",
+                    },
+                },
+            });
+        });
+}
+
+function editarEvento() {
+    console.log('Função de edição de evento ainda não implementada.');
 }
