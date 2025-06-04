03f93a681e8fba18d9e98d5463c2454511401a69
Murilo Teixeira <mutegbi@gmail.com>
2025-06-04 11:01:38 -0300

    Criado função para receber todos os motoristas e carros cadastrados e adicionar ao select

--- Front/assets/js/axios/gerarRealatorio.js
+++ Front/assets/js/axios/gerarRealatorio.js
@@ -12,4 +12,35 @@ $('#gerarRelatorio').on('click', function () {
 
 })
 
+function receberDados() {
+
+    const selectMotoristas = document.getElementById('selectMotoristas');
+    const selectCarros = document.getElementById('selectCarros');
+
+    axios.get(url + 'motoristas/') // Faz uma requisição GET para buscar os motoristas
+        .then(function (response) {
+            selectMotoristas.innerHTML = '<option value="">Selecione um motorista</option>';
+            response.data.forEach(function (motorista) {
+                selectMotoristas.innerHTML += `<option value="${motorista.id}">${motorista.nome}</option>`;
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao buscar motoristas:', error);
+        })
+
+        .then(function () {
+            axios.get(url + 'carros') // Faz uma requisição GET para buscar os carros
+                .then(function (response) {
+                    selectCarros.innerHTML = '<option value="">Selecione um carro</option>';
+                    response.data.forEach(function (carro) {
+                        selectCarros.innerHTML += `<option value="${carro.id}">${carro.modelo}</option>`;
+                    });
+                })
+                .catch(error => {
+                    console.error('Erro ao buscar carros:', error);
+                });
+        });
+}
+
+receberDados(); // Chama a função para preencher os selects ao carregar a página
 
