70e519bc11a56e1847eb2e5bc77e53c66ec82a40
Murilo Teixeira <mutegbi@gmail.com>
2025-06-09 13:21:19 -0300

    adicionado funnciolnalidades a pagina de relatorio

--- Back/src/routes/relatorios.js
+++ Back/src/routes/relatorios.js
@@ -1,29 +1,75 @@
 const express = require('express');
 const router = express.Router();
 const { Op } = require('sequelize');
+const sequelize = require('../config/sequelize'); // Importa a instância do Sequelize
 const Evento = require('../models/Eventos');
 
-// Relatório do motorista X
-router.get('/motorista/:id', async (req, res) => {
+
+// Relatório de todos os eventos
+router.get('/', async (req, res) => {
   try {
-    const eventos = await Evento.findAll({
-      where: { motoristaId: req.params.id }
-    });
+
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id ORDER BY Evento.data_inicial DESC;`,
+      {
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
     res.json(eventos);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-// Relatório do motorista X em uma data específica
+// Relatório do motorista X
+router.get('/motorista/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id WHERE motorista.id = ${id} ORDER BY Evento.data_inicial DESC;`,
+      {
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
+    res.json(eventos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Relatório do motorista X em um carro específico
+router.get('/motorista/:idm/carro/:idc', async (req, res) => {
+  try {
+    const { idm, idc } = req.params;
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id WHERE motorista.id = ${idm} AND carro.id = ${idc} ORDER BY Evento.data_inicial DESC;`,
+      {
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
+    res.json(eventos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Relatório do motorista X em uma data inicial
 router.get('/motorista/:id/data/:data', async (req, res) => {
   try {
-    const eventos = await Evento.findAll({
-      where: {
-        motoristaId: req.params.id,
-        data: req.params.data
+    const { id, data } = req.params;
+
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id 
+      WHERE id_motorista = :id_motorista 
+        AND data_inicial LIKE :data`,
+      {
+        replacements: {
+          id_motorista: id,
+          data: `${data}%` // Filtra eventos que começam com a data especificada
+        },
+        type: sequelize.QueryTypes.SELECT
       }
-    });
+    );
     res.json(eventos);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -31,18 +77,34 @@ router.get('/motorista/:id/data/:data', async (req, res) => {
 });
 
 // Relatório do motorista X entre duas datas
-// Ex: /relatorios/motorista/1/intervalo?inicio=2024-11-11&fim=2024-12-11
-router.get('/motorista/:id/intervalo', async (req, res) => {
+// Ex: /relatorios/motorista/1/2024-11-01/2024-11-30
+router.get('/motorista/:id/:dataInicial/:dataFinal', async (req, res) => {
   try {
-    const { inicio, fim } = req.query;
-    const eventos = await Evento.findAll({
-      where: {
-        motoristaId: req.params.id,
-        data: {
-          [Op.between]: [inicio, fim]
-        }
+    const { id, dataInicial, dataFinal } = req.params;
+
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id WHERE motorista.id = '${id}' AND Evento.data_inicial >= '${dataInicial}' AND Evento.data_final <= '${dataFinal}' OR motorista.id = '${id}' AND Evento.data_inicial >= '${dataInicial}' ORDER BY Evento.data_inicial DESC;`,
+      {
+        type: sequelize.QueryTypes.SELECT
       }
-    });
+    );
+    res.json(eventos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Relatório do motorista X em um carro específico e intervalo de datas
+router.get('/motorista/:idm/carro/:idc/:dataInicial/:dataFinal', async (req, res) => {
+  try {
+    const { idm, idc, dataInicial, dataFinal } = req.params;
+
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id WHERE motorista.id = '${idm}' AND carro.id = '${idc}' AND Evento.data_inicial >= '${dataInicial}' AND Evento.data_final <= '${dataFinal}' OR motorista.id = '${idc}' AND carro.id = '${idc}' AND Evento.data_inicial >= '${dataInicial}' ORDER BY Evento.data_inicial DESC;`,
+      {
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
     res.json(eventos);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -52,21 +114,68 @@ router.get('/motorista/:id/intervalo', async (req, res) => {
 //Relatório por carro
 router.get('/carro/:id', async (req, res) => {
   try {
-    const eventos = await Evento.findAll({
-      where: { carroId: req.params.id }
-    });
+    const { id } = req.params;
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id WHERE carro.id = ${id} ORDER BY Evento.data_inicial DESC;`,
+      {
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
     res.json(eventos);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-// Relatório por data "todos os eventos de uma data"
+// Relatório do carro X num intervalo data 
+router.get('/carro/:id/:dataInicial/:dataFinal', async (req, res) => {
+  try {
+    const { id, dataInicial, dataFinal } = req.params;
+
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id WHERE carro.id = '${id}' AND Evento.data_inicial >= '${dataInicial}' AND Evento.data_final <= '${dataFinal}' OR carro.id = '${id}' AND Evento.data_inicial >= '${dataInicial}' ORDER BY Evento.data_inicial DESC;`,
+      {
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
+    res.json(eventos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Relatório por data "todos os eventos de uma data inicial"
 router.get('/data/:data', async (req, res) => {
   try {
-    const eventos = await Evento.findAll({
-      where: { data: req.params.data }
-    });
+    const { data } = req.params;
+    console.log(`Buscando eventos para a data: ${data}`);
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id WHERE Evento.data_inicial LIKE :data ORDER BY Evento.data_inicial DESC;`,
+      {
+        replacements: {
+          data: `${data}%` // Filtra eventos que começam com a data especificada
+        },
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
+    res.json(eventos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Relatório por intervalo de datas
+// Exemplo: /relatorios/intervalo/2024-11-01/2024-11-30
+router.get('/intervalo/:dataInicial/:dataFinal', async (req, res) => {
+  try {
+    const { dataInicial, dataFinal } = req.params;
+
+    const eventos = await sequelize.query(
+      `SELECT Evento.id AS 'Evento.id', Evento.data_inicial AS 'Evento.data_inicial', Evento.data_final AS 'Evento.data_final', Evento.km_inicial AS 'Evento.km_inicial', Evento.km_final AS 'Evento.km_final', Evento.createdAt AS 'Evento.createdAt', Evento.updatedAt AS 'Evento.updateAt', motorista.id AS 'motorista.id', motorista.nome AS 'motorista.nome', motorista.status AS 'motorista.status', carro.id AS 'carro.id', carro.marca AS 'carro.marca', carro.modelo AS 'carro.modelo', carro.kmAtual AS 'carro.kmAtual', carro.status AS 'carro.status' FROM eventos AS Evento LEFT OUTER JOIN motoristas AS motorista ON Evento.id_motorista = motorista.id LEFT OUTER JOIN carros AS carro ON Evento.id_carro = carro.id WHERE Evento.data_inicial >= '${dataInicial}' AND Evento.data_final <= '${dataFinal}' ORDER BY Evento.data_inicial DESC;`,
+      {
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
     res.json(eventos);
   } catch (err) {
     res.status(500).json({ error: err.message });
--- Front/assets/js/axios/gerarRealatorio.js
+++ Front/assets/js/axios/gerarRealatorio.js
@@ -1,14 +1,178 @@
-$('#gerarRelatorio').on('click', function () {
-    console.log('gerarRelatorio');
-    $('#gerarRelatorio').removeClass('btn-success');
-    $('#gerarRelatorio').addClass('btn-warning');
-    $('#gerarRelatorio').html('<i class="fa fa-spinner fa-spin"></i> Gerando Relatório...');
-    setTimeout(function () {
-        $('#gerarRelatorio').addClass('btn-success');
-        $('#gerarRelatorio').removeClass('btn-warning');
-        $('#gerarRelatorio').html('<i class="fa fa-check"></i> GERAR');
+function formatarDataISO(dataISO) { // Função para formatar a data no formato "YYYY-MM-DD HH:mm:ss"
+    return dataISO.replace('T', ' ').split('.')[0]; // ex: "2025-05-27 14:00:00"
+}
+let t = 0;
+$('#gerarRelatorio').on('click', async () => {
 
-    }, 2000);
+    if (t == 1) {   // Verifica se já foi clicado
+        $('#relatorios-datatables').DataTable().clear().destroy(); // Destrói a tabela existente para evitar conflitos
+    }
+    if (t == 0) { t = 1 }
+    const motoristaId = document.getElementById('selectMotoristas').value;
+    const carroId = document.getElementById('selectCarros').value;
+    const dataInicial = document.getElementById('data_inicial').value;
+    const dataFinal = document.getElementById('data_final').value;
+
+    console.log('Motorista ID:', motoristaId);
+    console.log('Carro ID:', carroId);
+    console.log('Data Inicial:', dataInicial);
+    console.log('Data Final:', dataFinal);
+
+
+    if (new Date(dataInicial) > new Date(dataFinal)) {  // Verifica se a data inicial é menor que a data final
+        console.log('Data inicial é menor que a data final');
+        swal({
+            title: "Erro",
+            text: "A data inicial não pode ser maior que a data final.",
+            icon: "warning",
+            buttons: {
+                confirm: {
+                    className: "btn btn-danger",
+                },
+            },
+        });
+
+        return;
+    }
+    // Limpar tabela
+    const tbody = document.getElementById('body-table-relatorios');
+    const tthead = document.getElementById('thead-table-relatorios');
+    tthead.innerHTML = '';
+    tbody.innerHTML = '';
+
+    try {
+        let endpoint = '';
+        let response;
+
+        if (motoristaId && carroId && dataInicial && dataFinal) {
+            endpoint = `relatorios/motorista/${motoristaId}/carro/${carroId}/${dataInicial}/${dataFinal}`;
+        } else if (motoristaId && dataInicial && dataFinal) {
+            endpoint = `relatorios/motorista/${motoristaId}/${dataInicial}/${dataFinal}`;
+        } else if (carroId && dataInicial && dataFinal) {
+            endpoint = `relatorios/carro/${carroId}/${dataInicial}/${dataFinal}`;
+        } else if (motoristaId && carroId && dataInicial) {
+            endpoint = `relatorios/motorista/${motoristaId}/carro/${carroId}/data/${dataInicial}`;
+        } else if (motoristaId && dataInicial) {
+            endpoint = `relatorios/motorista/${motoristaId}/data/${dataInicial}`;
+        } else if (carroId && dataInicial) {
+            endpoint = `relatorios/carro/${carroId}`;
+            swal({
+                title: "Atenção",
+                text: "Você selecionou apenas um carro e data inicial. O relatório foi gerado para todas as datas desse carro. Se precisar de um intervalo, selecione uma data final, ou utilize o filtro dinâmico da tabela gerada.",
+                icon: "info",
+                buttons: {
+                    confirm: {
+                        className: "btn btn-info",
+                    },
+                },
+            });
+        } else if (motoristaId && carroId) {
+            endpoint = `relatorios/motorista/${motoristaId}/carro/${carroId}`;
+        } else if (dataInicial && dataFinal) {
+            endpoint = `relatorios/intervalo/${dataInicial}/${dataFinal}`;
+        } else if (motoristaId) {
+            endpoint = `relatorios/motorista/${motoristaId}`;
+        } else if (carroId) {
+            endpoint = `relatorios/carro/${carroId}`;
+        } else if (dataInicial) {
+            endpoint = `relatorios/data/${dataInicial}`;
+        } else if (dataFinal) {
+            endpoint = `relatorios/data/${dataFinal}`;
+        } else {
+            endpoint = `relatorios`
+        }
+        console.log('Endpoint:', endpoint);
+        // Exibe botão de carregamento
+        $('#gerarRelatorio').removeClass('btn-success').addClass('btn-warning').html('<i class="fa fa-spinner fa-spin"></i> Gerando Relatório...');
+
+        response = await axios.get(url + endpoint);
+
+        if (response.data.length === 0) {
+            tbody.innerHTML = `<tr><td colspan="4" class="text-center">Nenhum dado encontrado.</td></tr>`;
+        } else {
+            if (!motoristaId && carroId) {
+
+                tthead.innerHTML = `
+            <tr>                
+                <th>Veiculo</th>
+                <th>Data inicial</th>
+                <th>Data Final</th>
+                <th>km inicial</th>
+                <th>km Final</th>
+            </tr>
+            `;
+
+                response.data.forEach(evento => {
+                    let dataInicialIso = formatarDataISO(evento['Evento.data_inicial']);
+                    let dataFinalIso = 'em andamento'; // Valor padrão para data final
+                    if (evento['Evento.data_final']) {
+                        dataFinalIso = formatarDataISO(evento['Evento.data_final']);
+                    }
+                    tbody.innerHTML += `
+              <tr>                
+                <td>${evento['carro.marca'] || '---'} ${evento['carro.modelo'] || '---'}</td>
+                <td>${dataInicialIso}</td>
+                <td>${dataFinalIso}</td>
+                <td>${evento['Evento.km_inicial'] || '---'}</td>
+                <td>${evento['Evento.km_final'] || '---'}</td>                
+              </tr>
+            `;
+                });
+            } else {
+                tthead.innerHTML = `
+            <tr>  
+                <th>Motorista</th>              
+                <th>Veiculo</th>
+                <th>Data inicial</th>
+                <th>Data Final</th>
+                <th>km inicial</th>
+                <th>km Final</th>
+            </tr>
+            `;
+
+                response.data.forEach(evento => {
+                    let dataInicialIso = formatarDataISO(evento['Evento.data_inicial']);
+                    let dataFinalIso = 'em andamento'; // Valor padrão para data final
+                    if (evento['Evento.data_final']) {
+                        dataFinalIso = formatarDataISO(evento['Evento.data_final']);
+                    }
+                    tbody.innerHTML += `
+              <tr>
+                <td>${evento['motorista.nome'] || '---'}</td>                
+                <td>${evento['carro.marca'] || '---'} ${evento['carro.modelo'] || '---'}</td>
+                <td>${dataInicialIso}</td>
+                <td>${dataFinalIso}</td>
+                <td>${evento['Evento.km_inicial'] || '---'}</td>
+                <td>${evento['Evento.km_final'] || '---'}</td>                
+              </tr>
+            `;
+                });
+            }
+
+            $("#relatorios-datatables").DataTable({}); // Inicializa o DataTable após adicionar os dados
+
+
+            console.log(response.data);
+        }
+
+        // Restaura o botão
+        $('#gerarRelatorio').addClass('btn-success').removeClass('btn-warning').html('<i class="fa fa-check"></i> GERAR');
+
+    } catch (error) {
+        console.error('Erro ao gerar relatório:', error);
+        swal({
+            title: "Consulta não disponível",
+            text: "Não foi possivel gerar essa consulta. Se preferir limpe todos os campos para uma consulta geral e utilize o filtro dinâmico da tabela gerada.",
+            icon: "error",
+            buttons: {
+                confirm: {
+                    className: "btn btn-danger",
+                },
+            },
+        });
+        // Restaura o botão
+        $('#gerarRelatorio').addClass('btn-success').removeClass('btn-warning').html('<i class="fa fa-check"></i> GERAR');
+    }
 
 })
 
